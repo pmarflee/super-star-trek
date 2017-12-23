21ad1de0b3adf8f7fb79e18b207bc5df2e55fd91
Paul Marfleet <paul.marfleet@gmail.com>
2017-12-23 21:00:46 +0000

    Made Quadrant.klingons and Quadrant.hasStarbase parameter properties

--- src/game/quadrant.ts
+++ src/game/quadrant.ts
@@ -6,14 +6,14 @@ export default class Quadrant {
   public static readonly columns: number = 8;
   public static readonly rows: number = 8;
 
-  public klingons: number = 0;
-  public hasStarbase: boolean = false;
   public readonly sectors: Sector[];
 
   constructor(
     public readonly row: number,
     public readonly column: number,
-    public readonly stars: number) {
+    public readonly stars: number,
+    public klingons: number = 0,
+    public hasStarbase: boolean = false) {
 
     this.sectors = Array.from(new Array(Sector.rows), (row, rowIndex) => 
       Array.from(new Array(Sector.columns), (col, colIndex) => new Sector()));
